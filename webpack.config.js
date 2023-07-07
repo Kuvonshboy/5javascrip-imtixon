12b0b96d7229152e3c1b52bf420809c2b7ec72f5
kuvonshboy <bozorboyevquvonchbek0321@gmail.com>
2023-07-07 20:44:17 +0500

    tugatdim

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,66 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
 
 module.exports = {
-    mode: 'development',
-    entry:{
+    // mode
+    mode: 'production', // development
+    // entry
+    entry: {
         main: path.resolve(__dirname, 'src/js/main.js'),
         about: path.resolve(__dirname, 'src/js/about.js')
     },
-    output:{
+    // output
+    output : {
         path: path.resolve(__dirname, 'public'),
-        filename : '[name][contenthash].js',
+        filename: '[name][contenthash].js',
         clean: true
     },
-    plugins:[
+    // devServer
+    devServer: {
+        static: {
+            directory: path.resolve(__dirname, 'public')
+        },
+        port: 3000,
+        open:true,
+        hot: true,
+        compress: true,
+        historyApiFallback:true
+    },
+    // loader
+    module: {
+        rules:[
+            {
+                test: /\.css$/i,
+                use: [MiniCssExtractPlugin.loader, "css-loader"],
+            },
+            {
+                test: /\.m?js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                  loader: 'babel-loader',
+                  options: {
+                    presets: ['@babel/preset-env']
+                  }
+                }
+              }
+        ]
+    },
+    // plugins
+    plugins: [
         new HtmlWebpackPlugin({
-          title: "Countires | Home",
-          template:'./src/index.html',
-          filename: 'index.html',
-          chunks: ['main']
+         title: "Countires | Home",
+         template: './src/indexTemp.html',
+         filename: 'index.html',
+         chunks: ['main'] 
         }),
+        
         new HtmlWebpackPlugin({
-         title: "Countires | Abouta",
-         template:'./src/pages/about.html',
-         filename:'about.html', 
-         chunks: ['about']  
-        })
-
+            title: 'Countries | About',
+            template: './src/pages/aboutTemp.html',
+            filename: 'about.html',
+            chunks: ['about']
+        }),
+        new MiniCssExtractPlugin()
     ]
-    
 }
